e60ff3627525ad9fff1dd1551a4290d97d68ccd6
Lachicagladiadora <dev.roseny@gmail.com>
2025-05-16 19:28:38 -0500

    chore: add typescript

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,13 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, NOW } from "astro:db";
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    columns: {
+      id: column.number({ primaryKey: true }),
+      author: column.text(),
+      content: column.text({ optional: true }),
+      published: column.date({ default: NOW }),
+    },
+  },
 });
