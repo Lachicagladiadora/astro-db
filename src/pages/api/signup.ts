2b6e002da07b812ec6b01de9f57a948cc80fe5e8
Lachicagladiadora <dev.roseny@gmail.com>
2025-06-09 19:00:12 -0500

    wip: sign up

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -2,16 +2,27 @@ import type { APIRoute } from "astro";
 
 export const prerender = false;
 
-export const POST: APIRoute = async (params, request) => {
-  console.log({ params, request });
+export const POST: APIRoute = async ({ params, request }) => {
+  // const tt = await request.json();
+  // console.log({ tt });
+  // if (request.headers.get("Content-Type") === "application/json") {
+  const body = await request.json();
+  console.log({ params, request, body });
   //run middleware
   // region todo: get bady , send to bd , save and redirect
   // return new Response("User created", { status: 200 });
   return new Response(
     JSON.stringify({
       message: "User created!",
-      path: new URL(params.url.origin).pathname,
+      // path: new URL(params.url.origin).pathname,
     }),
     { status: 200, statusText: "User created" }
   );
+  // }
+  // return new Response(
+  //   JSON.stringify({
+  //     message: "are you crazy?, you don't send body correctly",
+  //   }),
+  //   { status: 500 }
+  // );
 };
--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,7 +1,12 @@
 export const addUser = async (params) => {
   await fetch("http://localhost:4321/api/signup", {
     method: "POST",
-    body: { ...params, userId: 3, userName: "lu", role: "seller" },
+    body: JSON.stringify({
+      ...params,
+      userId: 3,
+      userName: "lu",
+      role: "seller",
+    }),
   });
   console.log(
     { params },
