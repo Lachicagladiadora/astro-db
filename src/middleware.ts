6ea9553ca73d7a4ca54754f01ca3a6bca2ecbc02
Lachicagladiadora <dev.roseny@gmail.com>
2025-06-24 11:24:50 -0500

    chore: updtae middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,29 @@
 import jwt from "jsonwebtoken";
 import { SECRET } from "./constants";
+import { getOneHourAfterNow } from "./utils/date.utils";
 
 export const onRequest = (context, next) => {
   console.log("Hi! middleware");
   const access = context.cookies.get("accessCookies");
-  console.log({ access });
-  // add refresh
+  const refresh = context.cookies.get("refreshCookies");
+  console.log({ access, refresh });
   if (!access) return next();
-  const userData = jwt.verify(access.value, SECRET);
-  context.locals.user = userData;
+  if (!refresh) return next();
+  const userDataAccess = jwt.verify(access.value, SECRET);
+  const userDataRefresh = jwt.verify(refresh.value, SECRET);
+  if (!userDataAccess) {
+    const accessToken = jwt.sign({ userId: userDataRefresh.userId }, SECRET, {
+      expiresIn: "1h",
+    });
+    context.cookies.set("accessCookies", accessToken, {
+      expires: getOneHourAfterNow(),
+    });
+    context.locals.user = userDataAccess.userId;
+    return next();
+  }
+  context.locals.user = userDataAccess;
   //
-  console.log({ userData });
+  console.log({ userDataAccess, userDataRefresh });
   // here validate tockens, it's all for the protected
   return next();
 };
